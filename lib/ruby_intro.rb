50dd31857155669c1bc171edf3dae5bc87618406
EC2 Default User <ec2-user@ip-172-31-25-74.us-west-2.compute.internal>
2018-09-10 07:34:22 +0000

    Part 1 complete

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,15 +3,28 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  arr.reduce(0, :+)
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  if arr.length == 0 then
+    return 0
+  elsif arr.length == 1 then
+    return arr[0]
+  else
+    return arr.max(2).inject(0,:+)
+  end
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  hash = Hash.new()
+  for x in arr
+    if hash[n-x] != nil then
+      return true
+    end
+    hash[x]=true
+  end
+  return false
 end
 
 # Part 2
